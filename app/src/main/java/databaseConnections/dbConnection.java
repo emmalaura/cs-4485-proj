40eb164d4aef8f51d2aea5c49f3aad91f16ac432
Pranith Mullapudi <pmullapudi1@gmail.com>
2026-04-30 22:08:03 -0500

    Added connection pooling for dbConnection
    Previously on reports page, each entry calls a word query which generates a new connection. This quickly exhausts the OS's ephemeral ports. Added a connection pooling in dbConnection so that repeated connections reuse the same ports instead

--- app/src/main/java/databaseConnections/dbConnection.java
+++ app/src/main/java/databaseConnections/dbConnection.java
@@ -1,14 +1,27 @@
 package databaseConnections;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 public class dbConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/CS4485DB";
     private static final String USER = "javauser";
     private static final String PASSWORD = "cs4485";
+    private static HikariDataSource dataSource;
 
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(URL);
+        config.setUsername(USER);
+        config.setPassword(PASSWORD);
+        config.setMaximumPoolSize(10);
+        config.setMinimumIdle(5);
+        config.setConnectionTimeout(20000);
+        dataSource = new HikariDataSource(config);
+    }
     public static Connection getConnection() throws SQLException{
-        return DriverManager.getConnection(URL,USER,PASSWORD);
+        return dataSource.getConnection();
     }
 
     public static void main(String[] args){
